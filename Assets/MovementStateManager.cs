61016d674693779738fba402a9e9bf2f073c7e6a
VinNVu1 <vincentevu0989@icloud.com>
2024-12-31 16:03:58 -0700

    Added Gravity, GroundCheck

--- Assets/MovementStateManager.cs
+++ Assets/MovementStateManager.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +10,14 @@ public class MovementStateManager : MonoBehaviour
     CharacterController controller;
     InputSystemActions playerInput;
     InputAction moveAction;
+
+    [SerializeField] float groundYOffset;
+    [SerializeField] LayerMask groundMask;
+    Vector3 spherePos;
+
+    [SerializeField] float gravity = -9.81f;
+    Vector3 velocity;
+
 
     void Awake()
     {
@@ -37,6 +46,7 @@ public class MovementStateManager : MonoBehaviour
     void Update()
     {
         GetDirectionAndMove();
+        Gravity();
     }
 
     void GetDirectionAndMove()
@@ -45,5 +55,26 @@ public class MovementStateManager : MonoBehaviour
         direction = transform.forward * input.y + transform.right * input.x;
 
         controller.Move(direction * moveSpeed * Time.deltaTime);
+    }
+
+    bool isGrounded()
+    {
+        spherePos = new Vector3(transform.position.x - groundYOffset, transform.position.z);
+        if (Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask)) return true;
+        return false; 
+    }
+
+    void Gravity()
+    {
+        if (!isGrounded()) velocity.y += gravity * Time.deltaTime;
+        else if (velocity.y < 0) velocity.y = -2;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(spherePos, controller.radius - 0.05f);
     }
 }
